ab3217a821b58f37b3f04f77d7df6a885e911b09
Kye Buffery <kyebuffery14@gmail.com>
2018-10-30 21:54:13 +0000

    changed error catching

--- public/db-test.php
+++ public/db-test.php
@@ -2,7 +2,7 @@
 
 // Server is mysql docker container's IP address
 // Found by docker inspect <cid>
-$db_server = "172.18.0.3";
+$db_server = "172.18.0.4";
 $db_username = "test";
 $db_password = "test";
 $db_database = "test";
@@ -10,7 +10,7 @@ $db_database = "test";
 // Create connection
 $db_connection = new mysqli($db_server, $db_username, $db_password, $db_database);
 
-if (!$db_connection) {
+if (mysqli_connect_errno()) {
 	echo 'Connection failed. Check the README in order to setup the DB connection.';
 } else {
 	echo 'Connected successfully to MySQL container at ' . $db_server;
